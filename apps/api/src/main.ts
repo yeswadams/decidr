fd94269d23733921c3286e4a77f6c9758d83268b
yeswadams <yeswaadams73@gmail.com>
2026-09-10 04:32:24 +0300

    chore: monorepo setup using pnpm workspace and turbo repo

---
+++ apps/api/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule, ObserveInstrument } from './app.module.js';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    instrument: ObserveInstrument,
+  });
+  await app.listen(process.env.PORT ?? 4000);
+}
+await bootstrap();
