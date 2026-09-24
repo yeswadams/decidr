2af66ee37f43fbd877e42c0d41a807103c489070
yeswadams <yeswaadams73@gmail.com>
2026-09-24 22:29:11 +0300

    feature: Integrated nested fetches in the Nexstjs Application Factory

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,8 +4,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    instrument: ObserveInstrument,
-  });
+    instrument: ObserveInstrument, // handler config for observing routes in prod
+    routeConflictPolicy: {duplicate: 'error', shadow: 'warn'}, //  controlls registration order
+  }
+);
+  app.set("query parser", "extended");  // HTTP adapter to use extended parser for complex queries
   await app.listen(process.env.PORT ?? 4000);
 }
 await bootstrap();
